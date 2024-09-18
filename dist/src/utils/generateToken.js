877a2e7b372aeedcefcd4373bfd2b765f657e130
orijen-devs <bryanakere89@gmail.com>
2024-09-18 14:50:55 +0100

    updated dist  generateToken && main generateToken

--- dist/src/utils/generateToken.js
+++ dist/src/utils/generateToken.js
@@ -13,7 +13,7 @@ const generateToken = (res, user) => {
     res.cookie('acc_tk', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-        sameSite: 'strict', // Prevent CSRF attacks
+        sameSite: 'none', // Prevent CSRF attacks
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     });
 };
