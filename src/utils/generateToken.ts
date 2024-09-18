be7f0f99368959c52cf8dbd72c23c55314da0c0a
orijen-devs <bryanakere89@gmail.com>
2024-09-18 14:01:01 +0100

    updated generateToken

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -10,7 +10,7 @@ export const generateToken = (res: Response, user: string) => {
 
   res.cookie('acc_tk', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
+    secure: false, // Use secure cookies in production
     sameSite: 'strict', // Prevent CSRF attacks
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   }); 
